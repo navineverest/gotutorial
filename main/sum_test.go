61e644591367f41c11051b1c7af5a1fcc7594f8a
navineverest <navin.kumar@everest.engineering>
2020-09-08 16:43:00 +0530

    Sum full

--- main/sum_test.go
+++ main/sum_test.go
@@ -1,31 +1,52 @@
 package main
 
 import (
+	"reflect"
 	"testing"
 )
 
 func TestSum(t *testing.T) {
+	numbers := []int{1, 2, 3}
 
-	t.Run("collection of 5 numbers", func(t *testing.T) {
-		numbers := []int{1, 2, 3, 4, 5}
+	got := Sum(numbers)
+	want := 6
 
-		got := Sum(numbers)
-		want := 15
+	if got != want {
+		t.Errorf("got %d want %d given, %v", got, want, numbers)
+	}
+}
 
-		if got != want {
-			t.Errorf("got %d want %d given, %v", got, want, numbers)
+func TestSumAll(t *testing.T) {
+
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
+
+func TestSumTails(t *testing.T) {
+	checkSums := func(t *testing.T, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
 		}
+	}
+
+	t.Run("Slice tail test", func(t *testing.T) {
+
+		got := SumTails([]int{1, 2, 3}, []int{0, 9})
+		want := []int{5, 9}
+
+		checkSums(t, got, want)
 	})
 
-	t.Run("collection of any size", func(t *testing.T) {
-		numbers := []int{1, 2, 3}
+	t.Run("Slice empty list", func(t *testing.T) {
 
-		got := Sum(numbers)
-		want := 6
+		got := SumTails([]int{}, []int{0, 9})
+		want := []int{0, 9}
 
-		if got != want {
-			t.Errorf("got %d want %d given, %v", got, want, numbers)
-		}
+		checkSums(t, got, want)
 	})
-
 }
