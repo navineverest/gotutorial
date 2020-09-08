ec10eb757023760a94b9e6b7f5d383286cc2fc1e
navineverest <navin.kumar@everest.engineering>
2020-09-08 17:37:47 +0530

    Interfaces

--- main/shapes.go
+++ main/shapes.go
@@ -2,6 +2,10 @@ package main
 
 import "math"
 
+type Shape interface {
+	Area() float64
+}
+
 type Rectangle struct {
 	Width  float64
 	Height float64
@@ -17,4 +21,13 @@ type Circle struct {
 
 func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
+}
+
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return 0.5 * t.Base * t.Height
 }
