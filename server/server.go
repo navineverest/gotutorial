62a184fb6fb56675b55549cb25cf32ae16a1acad
navineverest <navin.kumar@everest.engineering>
2020-09-09 16:41:13 +0530

    Context

---
+++ server/server.go
@@ -0,0 +1,15 @@
+package server
+
+import (
+	"context"
+	"net/http"
+)
+
+type Store interface {
+	Fetch(ctx context.Context) (string, error)
+}
+
+func Server(store Store) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+	}
+}
